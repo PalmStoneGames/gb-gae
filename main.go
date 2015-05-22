37017235e9f87875bbd64855bede12f5f9c44628
Luna Duclos <luna.duclos@palmstonegames.com>
2015-05-22 15:52:52 +0200

    Add custom import path check

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package main // import "code.palmstonegames.com/gb-gae"
 
 import (
 	"errors"
